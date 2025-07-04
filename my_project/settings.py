851254235f9d3ae08e16405a142c570fe6a7a769
satyajitbarik12345 <bariksatyajit16@gmail.com>
2025-07-05 04:12:42 +0530

    fix: added render domain to ALLOWED_HOSTS

--- my_project/settings.py
+++ my_project/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-*7!!kc@bmtx8ngui6lr@xmifmcwm6y%hnbe)rdei(b!ds8t)uq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecommerce-backend-b0r3.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
