cbde77d301198a103dcdadf6870725634b021762
satyajitbarik12345 <bariksatyajit16@gmail.com>
2025-07-07 23:43:21 +0530

    added database

--- my_project/settings.py
+++ my_project/settings.py
@@ -90,10 +90,15 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Ecommerce',            # name of your db
+        'USER': 'postgres',             # username (default is postgres)
+        'PASSWORD': 'Prangya',    # password you created during install
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
