30d9560892c83bf6dbcebd543c7e4fa006ea9aa7
satyajitbarik12345 <bariksatyajit16@gmail.com>
2025-07-14 05:34:35 +0530

    Prepare for DigitalOcean deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -101,9 +101,12 @@ DATABASES = {
 }
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
+
 
 
 
